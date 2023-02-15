d20e5d3fbd125befbd72bc279ec48c8b229b53db
Andrew C. Ross <andrew.c.ross@noaa.gov>
2023-02-15 15:01:49 -0500

    Update code and build

--- builds/linux-build.bash
+++ builds/linux-build.bash
@@ -77,7 +77,9 @@ srcdir=$abs_rootdir/../src
 mkdir -p build/$machine_name-$platform/shared/$target
 pushd build/$machine_name-$platform/shared/$target   
 rm -f path_names                       
-$srcdir/mkmf/bin/list_paths $srcdir/FMS/{affinity,amip_interp,column_diagnostics,diag_integral,drifters,horiz_interp,memutils,sat_vapor_pres,topography,astronomy,constants,diag_manager,field_manager,include,monin_obukhov,platform,tracer_manager,axis_utils,coupler,fms,fms2_io,interpolator,mosaic,mosaic2,random_numbers,time_interp,tridiagonal,block_control,data_override,exchange,mpp,time_manager}/ $srcdir/FMS/libFMS.F90
+# $srcdir/mkmf/bin/list_paths $srcdir/FMS/{affinity,amip_interp,column_diagnostics,diag_integral,drifters,horiz_interp,memutils,parser,sat_vapor_pres,string_utils,topography,astronomy,constants,diag_manager,field_manager,include,monin_obukhov,platform,tracer_manager,axis_utils,coupler,fms,fms2_io,interpolator,mosaic,mosaic2,random_numbers,time_interp,tridiagonal,block_control,data_override,exchange,mpp,time_manager}/ $srcdir/FMS/libFMS.F90
+$srcdir/mkmf/bin/list_paths $srcdir/FMS/*/ $srcdir/FMS/libFMS.F90
+
 $srcdir/mkmf/bin/mkmf -t $abs_rootdir/$machine_name/$platform.mk -p libfms.a -c "-Duse_libMPI -Duse_netCDF -DMAXFIELDMETHODS_=400" path_names
 
 make $makeflags libfms.a         
@@ -93,8 +95,13 @@ if [[ $flavor =~ "mom6sis2" ]] ; then
     mkdir -p build/$machine_name-$platform/ocean_ice/$target
     pushd build/$machine_name-$platform/ocean_ice/$target
     rm -f path_names
-    $srcdir/mkmf/bin/list_paths $srcdir/MOM6/{config_src/infra/FMS2,config_src/memory/dynamic_symmetric,config_src/drivers/FMS_cap,config_src/external/ODA_hooks,config_src/external/stochastic_physics,config_src/external/drifters,config_src/external/database_comms,pkg/GSW-Fortran/{modules,toolbox}/,src/{*,*/*}/} $srcdir/SIS2/{config_src/dynamic,config_src/external/Icepack_interfaces,src} $srcdir/icebergs/ $srcdir/FMS/{coupler,include}/ $srcdir/{ocean_BGC/generic_tracers,ocean_BGC/mocsy/src}/ $srcdir/{atmos_null,ice_param,land_null,coupler/shared/,coupler/full/}/
+    # "mom6/src/MOM6/config_src/memory/dynamic_symmetric mom6/src/MOM6/config_src/drivers/FMS_cap mom6/src/MOM6/src/*/ mom6/src/MOM6/src/*/*/ mom6/src/MOM6/config_src/external/ODA_hooks mom6/src/MOM6/config_src/external/stochastic_physics mom6/src/MOM6/config_src/external/drifters mom6/src/MOM6/config_src/external/database_comms ocean_BGC/generic_tracers ocean_BGC/mocsy/src mom6/src/MOM6/pkg/GSW-Fortran/*/ mom6/src/MOM6/config_src/infra/FMS1">
+
+   #  $srcdir/mkmf/bin/list_paths $srcdir/MOM6/{config_src/infra/FMS1,config_src/memory/dynamic_symmetric,config_src/drivers/FMS_cap,config_src/external/ODA_hooks,config_src/external/stochastic_physics,config_src/external/drifters,config_src/external/database_comms,pkg/GSW-Fortran/{modules,toolbox}/,src/{*,*/*}/} $srcdir/SIS2/{config_src/dynamic,config_src/external/Icepack_interfaces,src} $srcdir/icebergs/ $srcdir/FMS/{coupler,include}/ $srcdir/{ocean_BGC/generic_tracers,ocean_BGC/mocsy/src}/ $srcdir/{atmos_null,ice_param,land_null,coupler/shared/,coupler/full/}/
+    $srcdir/mkmf/bin/list_paths  $srcdir/MOM6/config_src/memory/dynamic_symmetric $srcdir/MOM6/config_src/drivers/FMS_cap $srcdir/MOM6/src/*/ $srcdir/MOM6/src/*/*/ $srcdir/MOM6/config_src/external/ODA_hooks $srcdir/MOM6/config_src/external/stochastic_physics $srcdir/MOM6/config_src/external/drifters $srcdir/MOM6/config_src/external/database_comms $srcdir/{ocean_BGC/generic_tracers,ocean_BGC/mocsy/src}/  $srcdir/MOM6/pkg/GSW-Fortran/{modules,toolbox}/ $srcdir/MOM6/config_src/infra/FMS2 $srcdir/icebergs/ $srcdir/SIS2/{config_src/dynamic,config_src/external/Icepack_interfaces,src}  $srcdir/FMS/{coupler,include}/ $srcdir/{atmos_null,ice_param,land_null,coupler/shared/,coupler/full/}/
+
     $srcdir/mkmf/bin/mkmf -t $abs_rootdir/$machine_name/$platform.mk -o "-I../../shared/$target" -p MOM6SIS2 -l "-L../../shared/$target -lfms" -c '-DMAX_FIELDS_=100 -DNOT_SET_AFFINITY -D_USE_MOM6_DIAG -D_USE_GENERIC_TRACER  -DUSE_PRECISION=2 -D_USE_LEGACY_LAND_ -Duse_AM3_physics' path_names
+
 
 if [[ "$target" =~ "managedACC" ]] ; then 
     sed -e 's/-c\(.*\)COBALT/-acc -ta=nvidia:managed -Minfo=accel -c \1COBALT/' -i Makefile
